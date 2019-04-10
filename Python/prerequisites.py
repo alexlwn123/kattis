3a5aa62cad07328b5f236beb720f1409e11fff65
Alex Lewin <asl0028@auburn.edu>
2019-04-10 12:42:21 -0500

    fix rm

--- Python/prerequisites.py
+++
@@ -1,8 +0,0 @@
-def main():
-    line = input()
-    while line != '0':
-        k,m = map(int, line.split())
-        wants = list(map(int, input().split()))
-
-if __name__ == '__name__':
-    main()
