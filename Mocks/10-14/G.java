226095374cd602a0331954f0d1ec2b596a3e1cee
Alex Lewin <asl0028@auburn.edu>
2019-09-25 15:36:05 -0500

    clean

--- Mocks/10-14/G.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class G {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int x = input.nextInt();
-		System.out.println((int) (Math.pow(2, x) - x - 1));
-	}
-}
