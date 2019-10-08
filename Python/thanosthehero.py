3553efbce1615b9653f04de993511beb15e1039e
Alex Lewin <asl0028@auburn.edu>
2019-10-08 10:22:46 -0500

    10/8/19 Updates

--- Python/thanosthehero.py
+++ Python/thanosthehero.py
@@ -1,11 +1,13 @@
 def main():
   n = int(input())
-  arr = list(int, input().split())
-  arr = arr[::-1]
+  arr = list(map(int, input().split()))[::-1]
   count = 0
-  for i in range(n-1):
-    if arr[i] < arr[i+1]:
-      count += arr[i+1] - arr[i]
-
+  for i in range(1, n):
+    if arr[i] >= arr[i-1]:
+      count += arr[i] - arr[i-1] + 1
+      arr[i] = arr[i-1] - 1
+  if arr[-1] < 0:
+    count = 1
+  print(count)
 if __name__ == '__main__':
   main()
