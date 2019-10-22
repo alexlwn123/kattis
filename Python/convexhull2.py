a648aac88d14abc49dc1a675104ff4efe2511c04
Alex Lewin <asl0028@auburn.edu>
2019-10-22 15:23:11 -0500

    10/22/19 Updates

--- Python/convexhull2.py
+++ Python/convexhull2.py
@@ -1,50 +1,50 @@
-def main():
-    size = int(input())
-    points = []
-
-    for i in range(size):
-        line = input().split(' ')
-        if line[2] == 'Y':
-            p = (int(line[0]), int(line[1]))
-            points.append(p)
-    start = min(points, key=lambda p: (p[0], p[1]))
-    points.pop(points.index(start))
-
-    points = sorted(points, key=lambda p: (slope(start, p), p[0], p[1]))
-
-    points.insert(0, start)
-    out = '\n'.join(str(point[0]) + " " + str(point[1]) for point in points)
-
-    if len(points) > 1:
-        points = gScan(points)
-    print(len(points))
-    print(out)
-
 def gScan(points):
 
-    def cross(p1, p2, p3):
-        return (p2[0]- p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0])
+  def cross(p1, p2, p3):
+    return (p2[0]- p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0])
 
 
-    start = min(points, key=lambda p: (p[0],p[1]))
+  start = min(points, key=lambda p: (p[0],p[1]))
 
-    points.pop(points.index(start))
+  points.pop(points.index(start))
 
-    points.sort(key=lambda p: (slope(start, p), -p[1], p[0]))
+  points.sort(key=lambda p: (slope(start, p), -p[1], p[0]))
 
-    ans = [start]
-    for p in points:
-        ans.append(p)
-        while len(ans) > 2 and cross(ans[-3], ans[-2], ans[-1]) < 0:
-            ans.pop(-2)
+  ans = [start]
+  for p in points:
+    ans.append(p)
+    while len(ans) > 2 and cross(ans[-3], ans[-2], ans[-1]) < 0:
+      ans.pop(-2)
 
-    return ans
+  return ans
 
 
 def slope(p1, p2):
-    if p1[0] == p2[0]:
-        return float('inf')
-    return 1.0 * (p2[1] - p1[1]) / (p2[0] - p1[0])
+  if p1[0] == p2[0]:
+    return float('inf')
+  return 1.0 * (p2[1] - p1[1]) / (p2[0] - p1[0])
+
+def main():
+  size = int(input())
+  points = []
+
+  for i in range(size):
+    line = input().split(' ')
+    if line[2] == 'Y':
+      p = (int(line[0]), int(line[1]))
+      points.append(p)
+  start = min(points, key=lambda p: (p[0], p[1]))
+  points.pop(points.index(start))
+
+  points = sorted(points, key=lambda p: (slope(start, p)))
+
+  points.insert(0, start)
+
+  points = gScan(points)
+  out = '\n'.join(str(point[0]) + " " + str(point[1]) for point in points)
+  print(len(points))
+  print(out)
+
 """
 class Point:
 
@@ -57,6 +57,6 @@ class Point:
         return str(self.x) + " " + str(self.y)
 """
 if __name__ == '__main__':
-    main()
+  main()
 
 
