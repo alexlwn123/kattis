9cb458f30f5f0994556bc5a37903240de4f03ea9
Alex Lewin <asl0028@auburn.edu>
2020-05-19 21:53:51 -0400

    5/19/20 Updates

---
+++ Python/luckynumber.py
@@ -0,0 +1,31 @@
+count = 0
+def generate(num, k, MAX):
+  global count
+  if num % k != 0:
+    return
+  if k == MAX:
+    count += 1
+    return
+
+  for i in range(10):
+    nex = num * 10 + i
+    generate(nex, k+1, MAX)
+
+
+
+
+
+def main():
+  N = int(input())
+  global count
+  for i in range(1, 10):
+    generate(i, 1, N)
+
+  print(count)
+
+
+  
+
+
+if __name__ == '__main__':
+  main()
